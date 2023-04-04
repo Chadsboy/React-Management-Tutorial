49fb3a25030b6066b3f8559ebbe8bfe7ad69e9e7
Chadsboy <cdy8648@naver.com>
2023-04-04 19:06:50 +0900

    Create Customer.js

--- management/src/App.js
+++ management/src/App.js
@@ -1,15 +1,54 @@
-import logo from "./logo.svg";
+import React, { Component } from "react";
+import Customer from "./components/Customer";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="gray-background">
-        <img src={logo} lat="logo" />
-        <h2>Let's develop management system !</h2>
-      </header>
-    </div>
-  );
+const customers = [
+  {
+    id: 1,
+    image: "https://placeimg.com/64/64/1",
+    name: "최두영",
+    birthday: "860408",
+    gender: "남자",
+    job: "회사원",
+  },
+  {
+    id: 2,
+    image: "https://placeimg.com/64/64/2",
+    name: "송아영",
+    birthday: "840918",
+    gender: "여자",
+    job: "회사원",
+  },
+  {
+    id: 3,
+    image: "https://placeimg.com/64/64/3",
+    name: "최원",
+    birthday: "220103",
+    gender: "남자",
+    job: "귀염둥이",
+  },
+];
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {customers.map((c) => {
+          return (
+            <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+            />
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
