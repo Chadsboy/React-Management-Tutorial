ae6fbd983e9d24b9c61e518f8b0fb30a0900caca
Chadsboy <cdy8648@naver.com>
2023-04-05 09:44:47 +0900

    1_add material ui

--- management/src/App.js
+++ management/src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from "react";
 import Customer from "./components/Customer";
 import "./App.css";
+import Table from "@mui/material/Table";
+import TableHead from "@mui/material/TableHead";
+import TableBody from "@mui/material/TableBody";
+import TableRow from "@mui/material/TableRow";
+import TableCell from "@mui/material/TableCell";
 
 const customers = [
   {
@@ -33,19 +38,34 @@ class App extends Component {
   render() {
     return (
       <div>
-        {customers.map((c) => {
-          return (
-            <Customer
-              key={c.id}
-              id={c.id}
-              image={c.image}
-              name={c.name}
-              birthday={c.birthday}
-              gender={c.gender}
-              job={c.job}
-            />
-          );
-        })}
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {customers.map((c) => {
+              return (
+                <Customer
+                  key={c.id}
+                  id={c.id}
+                  image={c.image}
+                  name={c.name}
+                  birthday={c.birthday}
+                  gender={c.gender}
+                  job={c.job}
+                />
+              );
+            })}
+            ;
+          </TableBody>
+        </Table>
       </div>
     );
   }
